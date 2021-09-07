2a1bdd530a41a352a69646ebda27a7bf3fa8fc76
Cooper Barnes <cooperbrns@gmail.com>
2021-09-07 18:44:59 -0400

    cbarnes commit

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//For directing to next page on save meeting button
+window.location.href = "meeting.html";
+
+
+// Adding and Removing items on notes
+
+const inputText = document.getElementById("txt");
+const myButton = document.getElementsByClassName("insert-below-butto");
