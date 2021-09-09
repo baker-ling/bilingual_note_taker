4818c5f459028e4c1a46ead000853ca4da10c287
Brian D. Baker <brian.baker.bdb@gmail.com>
2021-09-09 19:49:55 -0400

    Moved utility code to utils.js and linked separate js files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,4 @@
-// Global constants for various keys
-const PAST_MEETINGS_LS_KEY = "past_meetings"; // TODO: Make sure same keys is used for storing past meeting metadata in local storage
-const PAST_MEETING_ID_SEARCH_PARAM_KEY = "past_meeting_name";
+
 
 //For directing to next page on save meeting button
 // window.location.href = "meeting.html";
---
+++ assets/js/utils.js
@@ -0,0 +1,78 @@
+// Global constants for various keys
+const PAST_MEETINGS_LS_KEY = "past_meetings"; // TODO: Make sure same keys is used for storing past meeting metadata in local storage
+const PAST_MEETING_ID_SEARCH_PARAM_KEY = "past_meeting_name";
+
+// objects for converting langauge codes to names and vice-versa
+const langCodesToNames = {
+    "en": "English",
+    "ar": "Arabic",
+    "zh": "Chinese",
+    "fr": "French",
+    "de": "German",
+    "hi": "Hindi",
+    "id": "Indonesian",
+    "ga": "Irish",
+    "it": "Italian",
+    "ja": "Japanese",
+    "ko": "Korean",
+    "pl": "Polish",
+    "pt": "Portuguese",
+    "ru": "Russian",
+    "es": "Spanish",
+    "tr": "Turkish",
+    "vi": "Vietnamese"
+  };
+  
+  const langNamesToCodes = {
+    "English": "en",
+    "Arabic": "ar",
+    "Chinese": "zh",
+    "French": "fr",
+    "German": "de",
+    "Hindi": "hi",
+    "Indonesian": "id",
+    "Irish": "ga",
+    "Italian": "it",
+    "Japanese": "ja",
+    "Korean": "ko",
+    "Polish": "pl",
+    "Portuguese": "pt",
+    "Russian": "ru",
+    "Spanish": "es",
+    "Turkish": "tr",
+    "Vietnamese": "vi"
+  };
+
+  
+/**
+ * Gets a machine translation from libretranslate.de
+ * @param {string} sourceText - Text to be translated
+ * @param {string} sourceLanguage - Language of text to be translated
+ * @param {string} targetLanguage - Language to be translated into
+ * @returns {string} Machine translation result.
+ */
+async function getTranslation(sourceText, sourceLanguage, targetLanguage) {
+    const requestResult = await fetch("https://libretranslate.de/translate", {
+      method: "POST",
+      body: JSON.stringify({
+        q: sourceText,
+        source: sourceLanguage,
+        target: targetLanguage,
+      }),
+      headers: { "Content-type": "application/json" },
+    });
+  
+    const data = await requestResult.json();
+    return data.translatedText;
+  }
+
+
+  function getSourceLanguage() {
+    // TODO implement proper logic for getSourceLanguage
+    return "en";
+  }
+  
+  function getTargetLanguage() {
+    // TODO implement proper logic for getTargetLanguage
+    return "es";
+  }
