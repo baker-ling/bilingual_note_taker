0fe03a17d40efab90a90867c76f3b0926d64cf87
Cooper Barnes <cooperbrns@gmail.com>
2021-09-11 10:19:01 -0400

    updated from last night

--- assets/js/meeting.js
+++ assets/js/meeting.js
@@ -1,7 +1,15 @@
 const mainElement = document.querySelector('main');
+<<<<<<< HEAD
 console.log(mainElement)
 
+<<<<<<< Updated upstream
 
+=======
+const 
+=======
+
+>>>>>>> 07d7feb0a21ae5453043446b2166fc364d1511b4
+>>>>>>> Stashed changes
 /**
  * Callback function for original note textareas to automatically
  * translate contents.
@@ -44,6 +52,14 @@ function mainElementonclickListener(event) {
   console.log(event)
   if (event.target.classList.contains("insert-below-button")) {
     let currentRow = event.target;
+<<<<<<< Updated upstream
+=======
+<<<<<<< HEAD
+    console.log(currentRow);
+    // while (currentRow.tagName !== "div") {
+    //   currentRow = currentRow.parentElement;
+    // }
+>>>>>>> Stashed changes
     currentRow = event.target.closest("div");
     insertRow(currentRow);
   } 
@@ -54,6 +70,20 @@ function mainElementonclickListener(event) {
   }
 }
 mainElement.addEventListener("click", mainElementonclickListener);
+<<<<<<< Updated upstream
+=======
+=======
+    currentRow = event.target.closest("div");
+    insertRow(currentRow);
+  } 
+  else if(event.target.classList.contains("delete-button")) {
+    let currentRow = event.target;
+    currentRow = event.target.closest("div");
+    deleteRow(currentRow);
+  }
+}
+mainElement.addEventListener("click", mainElementonclickListener);
+>>>>>>> Stashed changes
 
 
 function deleteRow(currentRow) {
@@ -62,6 +92,7 @@ function deleteRow(currentRow) {
 }
 
 
+<<<<<<< Updated upstream
 
 
 
@@ -71,6 +102,9 @@ function deleteRow(currentRow) {
 
 
 
+=======
+>>>>>>> 07d7feb0a21ae5453043446b2166fc364d1511b4
+>>>>>>> Stashed changes
 /**
  * Translates text in the given source text textarea
  * to its corresponding translation text textarea.
@@ -101,72 +135,33 @@ function getTargetTextarea(sourceTextarea) {
   return sourceTextarea.parentElement.querySelector(".note-translation");
 }
 
-
-
 //Exit button bottom redirecting to past_meetings.html
+<<<<<<< HEAD
 // const bottomExitbutton = document.getElementById("exit-button-bottom");
 // bottomExitbutton.addEventListener("click"),
 //   function () {
 //     location.href = "./past_meetings.html";
 //   };
+<<<<<<< Updated upstream
+=======
+=======
+>>>>>>> Stashed changes
 const bottomExitButton = document.getElementById("exit-button-bottom");
 bottomExitButton.addEventListener("click",
   function () {
     location.href = "./past_meetings.html";
   });
+<<<<<<< Updated upstream
+=======
+>>>>>>> 07d7feb0a21ae5453043446b2166fc364d1511b4
+>>>>>>> Stashed changes
 
 let saveMeetingEl = document.querySelector("#save-button-bottom");
-//global
-let pantryId = "238364cb-50ff-45e2-8f91-9e2d44b71215";
-let meetingsArr = JSON.parse(localStorage.getItem("meetingsArr")) || [];
-//code to shuffle array
-function shuffleArray(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
-}
-//code to create an identifier
-function createIdentifier() {
-  let arr = [];
-  for (let i = 0; i <= 2; i++) {
-    let lower = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-    let upper = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-    let num = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-    arr.push(lower, upper, num);
-  }
-  shuffleArray(arr);
-  return arr.join("");
-}
 
-//save the return from function to a variable
-let identifier = createIdentifier();
-console.log(identifier);
+let pantryKey = "238364cb-50ff-45e2-8f91-9e2d44b71215";
 
-//calls functions to save past meeting obj to localStorage and Pantry then redirects to past_meeting.html
-function save() {
-  saveToLocal();
-  saveToPantry();
-}
-
-//function to save to localStorage
-function saveToLocal() {
-  let meetingMetadata = {
-    //To add variables
-    title: "",
-    lastUpdated: "",
-    sourceLang: "",
-    targetLanguage: "",
-    pantryId: identifier,
-  };
-  meetingsArr.push(meetingMetadata);
-  localStorage.setItem("meetingsArr", JSON.stringify(meetingsArr));
-  location.assign("./past_meetings.html");
-  displayPastMeetingsList();
-}
-saveMeetingEl.addEventListener("click", save);
+//add event listener to save meeting button
+saveMeetingEl.addEventListener("click", saveToPantry);
 
 //function to POST to Pantry
 async function saveToPantry() {
@@ -182,8 +177,9 @@ async function saveToPantry() {
     }
   );
   const data = await response.text();
-  console.log(data);
+  location.assign("./past_meetings.html");
 }
+
 //function to DELETE from Pantry
 async function deleteNotes() {
   const response = await fetch(
